226530453c4b91144bbbba58ae6bc7c169ecaf37
AlekseyKolbasov <90603586+AlekseyKolbasov@users.noreply.github.com>
2021-11-10 20:49:58 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -9,22 +9,22 @@ int main()
 	ifstream infile("in.txt");
 
 	int x, y;
-	
+
 	int xleft=0,yleft=0,xright=0,yright=0;
-	
+
 	double cosright, cosleft;
 	cosleft = 1; cosright = 1;
 
 	int x0, y0;
 	infile >> x0 >> y0;
-	
-	
+
+
 	while (infile >> x >> y)
 	{
 		
 		
 		double cos = (x0*x+y0*y)/(sqrt(x*x+y*y)*sqrt(x0*x0+y0*y0));
-        	cos =round(cos * 10000000000.0) / 10000000000.0;
+        	cos =round(cos * 1000000.0) / 1000000.0;
 		if (cos <=cosleft && (y0*x < x0*y)) 
 		{
 			cosleft = cos;
