f3edd3bd408068cc5712f061485ec00a1e4bbba2
AlekseyKolbasov <90603586+AlekseyKolbasov@users.noreply.github.com>
2021-12-03 14:41:40 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -4,16 +4,19 @@
 #include <string>
 using namespace std;
 
-float traectoriaR(double g, double x, double xi, double yi, double V_x, double V_y){
-    float Y = - g*(x-xi)*(x-xi)/2/V_x/V_x+(V_y-g*xi/V_x)*(x-xi)/V_x+yi;
+double traectoriaR(double g, double x, double xi, double yi, double V_x, double V_y){
+    double Y = - g*(x-xi)*(x-xi)/2/V_x/V_x+(V_y-g*xi/V_x)*(x-xi)/V_x+yi;
+    return Y;
 }
-float traectoriaL(double g, double x, double xi, double yi, double V_x, double V_y){
-    float Y = - g*(x-xi)*(x-xi)/2/V_x/V_x-(V_y-g*xi/V_x)*(x-xi)/V_x+yi;
+double traectoriaL(double g, double x, double xi, double yi, double V_x, double V_y){
+    double Y = - g*(x-xi)*(x-xi)/2/V_x/V_x-(V_y-g*xi/V_x)*(x-xi)/V_x+yi;
+    return Y;
 }
-int proverkaRight(vector<double> X,vector<double> Y,double g, double xi, double yi, double V_x, double V_y,int zona,int a){
+int proverkaRight(vector<double> X,vector<double> Y,double g, double yi, double V_x, double V_y,int zona,int a){
     int n;
     for (int i = zona; i<=a-1; ++i){
         double l = - g*(X[i] - X[zona-1])*(X[i]-X[zona-1])/2/V_x/V_x+(V_y-g*X[zona-1]/V_x)*(X[i]-X[zona-1])/V_x+yi;
+        //cout<<l<<endl;
         if (l<0) {
             return 0;
         }
@@ -45,7 +48,7 @@ int main(int argc, char** argv) {
 
     ifstream file(argv[1]);
     double  h,V_x,V_y,x,y;
-    int a=1;
+    int a;
     vector<double>  X,Y;
 
     if (file.is_open()) {
@@ -57,8 +60,10 @@ int main(int argc, char** argv) {
             X.push_back(x);
             Y.push_back(y);
             a++;
+            //cout<<a<<endl;
         }
     }
+    //cout<<sizeof(X)/8-1<<endl;
         float g = 9.81; float xi = 0;
 
         int  zona = 1;
@@ -72,9 +77,9 @@ int main(int argc, char** argv) {
                 cout<<zona-1<<endl;
                 return 0;
             }
-            zona1 = proverkaRight(X, Y,g,xi , yi, V_x, V_y,zona,a);
-            if (zona1==sizeof(X)/8-1){
-                cout<<zona1-1<<endl;
+            zona1 = proverkaRight(X, Y,g , yi, V_x, V_y,zona, a);
+            if (zona1==a){
+                cout<<zona1<<endl;
                 return 0;
             }
             double yl = traectoriaR(g,X[zona1],X[zona-1],yi,V_x, V_y);
