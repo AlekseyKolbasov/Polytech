3138572d36caaefc7f4a49da754fc04d38d55d93
AlekseyKolbasov <90603586+AlekseyKolbasov@users.noreply.github.com>
2021-12-06 19:35:59 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -44,73 +44,7 @@ int proverkaleft(vector<double> X,vector<double> Y,double g, double yi, double V
 
 int main(int argc, char** argv) {
     if (argc==2) {
-        int zona = 1;
-        int zona1 = 0;
-
-        ifstream file(argv[1]);
-        double h, V_x = 0, V_y, x, y;
-        int a = 0;
-        vector<double> X, Y;
-        double g = 9.81, check, tmp;
-        if (file.is_open()) {
-            file >> h;
-            file >> V_x;
-            file >> V_y;
-            X.push_back(0);
-            Y.push_back(h);
-            double t_end = (V_y + sqrt(V_y * V_y + 2 * g * h)) / g;
-
-            while ((!file.eof()) && (check <=V_x * t_end)) {
-                if (file >> check >> tmp) {
-
-                    X.push_back(check);
-                    Y.push_back(tmp);
-                    a++;
-                }
-            }
-        }
-
-        if (V_x == 0) {
-            cout << zona1 << endl;
-            return 0;
-        }
-
-        double yi;
-        yi = Y[0];
-
-        while (yi >= 0) {
-            if (zona1 == zona) {
-                cout << zona1 << endl;
-                return 0;
-            }
-
-            zona1 = proverkaRight(X, Y, g, yi, V_x, V_y, zona, a);
-
-            if (zona1 == a ) {
-                cout << zona1  << endl;
-                return 0;
-            }
-
-            yi = traectoriaR(g, X[zona1+1], X[zona - 1], yi, V_x, V_y);
-
-            if (yi < 0) {
-                cout << int(zona1) << endl;
-                return 0;
-            }
-
-            zona = proverkaleft(X, Y, g, yi, V_x, V_y, zona1);
-
-            if (zona == 0) {
-                cout << zona << endl;
-                return 0;
-            }
-
-            yi = traectoriaL(g, X[zona], X[zona1+1], yi, V_x, V_y);
-
-            if (yi<0){
-                cout<<zona<<endl;
-                return 0;
-            }
-        }
+        cout<<1<<endl;
     }
+    
 }
