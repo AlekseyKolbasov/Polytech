7fa43902bc622bd795e74716298972088e918e5a
AlekseyKolbasov <90603586+AlekseyKolbasov@users.noreply.github.com>
2021-12-23 21:15:01 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -72,17 +72,19 @@ std::vector<double> coords(std::vector<double> X,double vx, double vy, double h,
 
 
 
-int main () {
+int main(int argc,char** argv) {
 
-    if (2 == 2) {
-        std::vector<double> ALL = ReadFile("test1_1.txt");
-        //std::cout<<ALL.size()<<std::endl;
+    if (argc == 2) {
+//        std::cout << "1st argument: " << argv[1] << std::endl;
+
+        std::vector<double> ALL = ReadFile(argv[1]);
 
         double h = ALL[0];
         double vx = ALL[1];
         double vy = ALL[2];
         double g = 9.81;
 
+//        std::cout << "h = " << h << " vx = " << vx << " vy = " << vy << std::endl;
         ALL.erase(ALL.begin() + 0);
         ALL.erase(ALL.begin() + 0);
         ALL.erase(ALL.begin() + 0);
@@ -97,13 +99,15 @@ int main () {
         std::vector<double> X_per;//X координата перегородки
         std::vector<double> X_rec;//X координата перегородок, от которых мячик отразися
 
-
         X_per = XorY(ALL, 1);
         Y_per = XorY(ALL, 2);
 
-        //std::cout<<X_per.size()<<std::endl;
+//        for (int i = 0; i<X_per.size();i++){
+//            std::cout << "X_per= " << X_per[i] << "Y_per = " << Y_per[i] << std::endl;
+//        }
 
         for (int i = 0; i < X_per.size(); i++) {
+//            std:: cout <<"coords X and Y is (" << X_per[i] << ", " << coords(X_per,vx,vy,h,g,n,X_rec)[i]<< ")" <<std::endl;
             Y_true.push_back(coords(X_per, vx, vy, h, g, n, X_rec)[i]);
         }
 
@@ -120,16 +124,21 @@ int main () {
         }
 
         if (way == 1) {
-            std::cout << X_per.size()<<std::endl;//ans
+            std::cout << X_per.size(); //ans
             return 0;
         }
+
+//        std::cout << "n_p = " << n_p << " Y(n_p) = " << Y_true[n_p] << " way is " << way << " X_rec " << X_rec[n_p] << std::endl;
+//        std::cout << "new vx";
+//        std::cout << "new Left = 0 and rght = 1";
         while (true) {
             if (way == 0) {
                 Y_true = coords(X_per, vx, vy, h, g, n, X_rec);
                 for (int i = n_p - 1; i >= 0; i--) {
 
                     if (Y_true[i] <= Y_per[i]) {
-
+//                        std::cout << "dvgalis vpravo";
+//                        std::cout << "Coords of parabola Y " << Y_true[i] << " coords of peregorodok is" << Y_per[i] <<std::endl;
                         n_p = i;
                         way = 1;
                         n++;
@@ -140,21 +149,24 @@ int main () {
                 }
 
                 if (Y_true[n_p] < 0) {
-                    int popka = n_p + 1;
-                    std::cout << popka<<std::endl; //ans
+                    n_p = n_p +1;
+                    std::cout << n_p ; //ans
                     return 0;
                 }
 
                 if (way == 0) {
-                    std::cout <<0<<std::endl; //ans
+                    std::cout << 0; //ans
                     return 0;
                 }
             }
+
             if (way == 1) {
                 Y_true = coords(X_per, vx, vy, h, g, n, X_rec);
                 for (int i = n_p + 1; i < X_per.size(); i++) {
 
                     if (Y_true[i] <= Y_per[i]) {
+//                        std::cout << "dvgalis vpravo";
+//                        std::cout << "Coords of parabola Y " << Y_true[i] << " coords of peregorodok is" << Y_per[i] <<std::endl;
                         n_p = i;
                         way = 0;
                         n++;
@@ -165,15 +177,21 @@ int main () {
                 }
 
                 if (Y_true[n_p] < 0) {
-                    std::cout <<n_p<<std::endl; //ans
+                    std::cout << n_p; //ans
                     return 0;
                 }
 
                 if (way == 1) {
-                    std::cout << X_per.size()<<std::endl; //ans
+                    std::cout << X_per.size(); //ans
                     return 0;
                 }
             }
         }
+
+//    } else {
+//        std::cerr << "No file" << std::endl;
+//        return 1;
+//    }
+//    std::cout << "n_p = " << n_p << " Y(n_p) = " << Y_true[n_p] << " way is " << way << " X_rec " << X_rec[n_p] << std::endl;
     }
 }
